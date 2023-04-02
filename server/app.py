ec4e969248b27ba2c0b551ade4b58378ab9c40e6
Saki <sbasken@gmail.com>
2023-04-02 11:58:26 -0700

    set up Flask routes in app.py and pass all pytests

--- server/app.py
+++ server/app.py
@@ -4,5 +4,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:string>')
+def print_string(string):
+    return f'<h1>{string}</h1>'
+
+@app.route('/count/<int:num>')
+def count(num):
+    display = f''
+    for n in range(num):
+        display += f'{n}\n'
+    return display
+
+@app.route('/math/<int:num1><string:operation><int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
+        
+@app.route('/print/hello')
+def hello():
+    print('hello')
+    return 'hello'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
